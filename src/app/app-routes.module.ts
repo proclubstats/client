5adcabb755b3b9e4cf97a76cd4b90280982f7e79
vAKN1N <aviezervaknin@gmail.com>
2024-02-11 13:23:30 +0200

    added bootstrap and template components

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -1,6 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
+import { FixturesComponent } from './components/fixtures/fixtures.component';
+import { LeagueTableComponent } from './components/league-table/league-table.component';
+import { TopScorersComponent } from './components/top-scorers/top-scorers.component';
+import { TopAssistsComponent } from './components/top-assists/top-assists.component';
 
 const routes: Routes = [
     {
@@ -9,6 +13,22 @@ const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent
+    },
+    {
+        path: 'fixtures',
+        component: FixturesComponent
+    },
+    {
+        path: 'top-assists',
+        component: TopAssistsComponent
+    },
+    {
+        path: 'top-scorers',
+        component: TopScorersComponent
+    },
+    {
+        path: 'league-table',
+        component: LeagueTableComponent
     }
 ];
 
